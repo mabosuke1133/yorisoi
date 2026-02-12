534ecbe1d5444eda4742fd7c11e7aa72b4acd185
Ubuntu <245080863+mabosuke1133@>
2026-02-12 13:03:51 +0000

    コントローラで「箱」を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,7 @@
 class PostsController < ApplicationController
+  # ログインしていないと投稿できないようにする
+  before_action :authenticate_user!
+  
   def index
   end
 
@@ -9,5 +12,23 @@ class PostsController < ApplicationController
   end
 
   def new
+    @post = Post.new # 新しい投稿用の空のインスタンスを作成
+  end
+
+  def create
+    @post = Post.new(post_params)
+    @post.user_id = current_user.id # ログイン中のユーザーIDを紐付け
+    if @post.save
+      redirect_to posts_path, notice: "寄り添い（投稿）を公開しました！"
+    else
+      render :new # 保存失敗時は新規登録画面を再表示
+    end
+  end
+
+  private
+
+  # 悪意のあるデータ操作を防ぐ「ストロングパラメーター」
+  def post_params
+    params.require(:post).permit(:title, :body, :emotion_level)
   end
 end
