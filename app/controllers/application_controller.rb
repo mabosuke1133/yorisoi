b766b275026880bd0ef4d8e9b4fe1341cb948356
Ubuntu <245080863+mabosuke1133@>
2026-02-13 12:28:35 +0000

    名前の編集が可能

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # 新規登録時にnameを許容
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    # 情報更新時にnameを許容
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
+  end
 end
