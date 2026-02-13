4187c626e576194b0f430c8c10a42d6438454855
Ubuntu <245080863+mabosuke1133@>
2026-02-13 12:31:13 +0000

    実装フェーズ1完了

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,3 +10,10 @@ class ApplicationController < ActionController::Base
     devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
+
+private
+
+  # 退会（アカウント削除）後に新規登録画面へ飛ばす
+  def after_sign_out_path_for(resource_or_scope)
+    new_user_registration_path
+  end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  # dependent: :destroy をつけることで、退会時に投稿も一緒に消えます
+  has_many :posts, dependent: :destroy
 end
