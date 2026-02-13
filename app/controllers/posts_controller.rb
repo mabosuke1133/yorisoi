e3909fec579a2d1b1ef124e9e0dd6f00ddad9936
Ubuntu <245080863+mabosuke1133@>
2026-02-13 12:15:18 +0000

    CRUD機能の実装

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -11,6 +11,11 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  # 【要件10】投稿者本人でない場合は、一覧へリダイレクトさせる（URL直打ち対策）
+   if @post.user != current_user
+     redirect_to posts_path, alert: "他人の投稿は編集できません。"
+   end
   end
 
   def new
@@ -18,12 +23,33 @@ class PostsController < ApplicationController
   end
 
   def create
-    @post = Post.new(post_params)
-    @post.user_id = current_user.id # ログイン中のユーザーIDを紐付け
-    if @post.save
-      redirect_to posts_path, notice: "寄り添い（投稿）を公開しました！"
+   @post = Post.new(post_params)
+   @post.user_id = current_user.id # ログイン中のユーザーIDを紐付け
+   if @post.save
+     redirect_to posts_path, notice: "寄り添い（投稿）を公開しました！"
+   else
+     render :new # 保存失敗時は新規登録画面を再表示
+   end
+  end
+  
+  def update
+    @post = Post.find(params[:id])
+    # 【要件7 & 8】バリデーションチェック。成功すれば詳細へ、失敗すれば編集画面を再表示
+    if @post.update(post_params)
+     redirect_to post_path(@post), notice: "投稿を更新しました！"
     else
-      render :new # 保存失敗時は新規登録画面を再表示
+     render :edit
+   end
+  end
+
+  def destroy
+    @post = Post.find(params[:id])
+    # 本人確認（要件10の削除制限も兼ねる）
+    if @post.user == current_user
+      @post.destroy
+      redirect_to posts_path, notice: "投稿を削除しました。"
+    else
+      redirect_to posts_path, alert: "他人の投稿は削除できません。"
     end
   end
 
