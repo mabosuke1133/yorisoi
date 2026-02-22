c2b64a35e5ebde0589bf462d20bcff96f0bb4ff3
Ubuntu <245080863+mabosuke1133@>
2026-02-22 04:58:32 +0000

    ルーティングの優先順位を修正し、newアクションが正しく動くように改善

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: 'homes#top' 
+  root to: 'homes#top'
   get 'about' => 'homes#about'
 
   devise_for :admins
@@ -7,5 +7,10 @@ Rails.application.routes.draw do
 
   resources :users, only: [:show, :edit, :update]
 
+  # 1. resources を先に書く（これで /posts/new が正しく判定されます）
   resources :posts
+
+  # 2. その後に、独自の削除ルートを書く
+  # 名前が被らないように、URLの最後に /sakujo をつけます
+  get 'posts/:id/sakujo' => 'posts#destroy', as: 'destroy_post'
 end
