20d7255a5ba69240ce8cbaf3b21a01d9c04b334a
Ubuntu <245080863+mabosuke1133@>
2026-03-27 12:21:02 +0000

    トップページの内容を再度変更

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,9 +4,4 @@ class HomesController < ApplicationController
 
   def about
   end
-
-  def home
-    # 投稿を作成日時の新しい順に並べて、最初から3つだけ取得する
-    @latest_posts = Post.order(created_at: :desc).limit(3)
-  end
 end
