95d2c8831a4ac9ee8f442d53fad02efba40adc96
Ubuntu <245080863+mabosuke1133@>
2026-03-28 11:43:48 +0000

    [fix] bootstrap files to webpacker

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -5,6 +5,11 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 
+import "jquery";
+import "popper.js";
+import "bootstrap";
+import "../stylesheets/application"; 
+
 window.Rails = Rails;
 if (Rails.root == null) {
   Rails.start();
